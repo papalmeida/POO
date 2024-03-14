24275fd684d1e5b9f36b52a36cd63143a854710a
Pedro Augusto <pedro.augusto120105@gmail.com>
2024-03-14 19:11:57 -0300

    new file:   Lista 1/.vscode/launch.json 	modified:   Lista 1/bin/Ex1.class 	new file:   Lista 1/bin/Main.class 	modified:   Lista 1/src/Ex1.java 	new file:   Lista 1/src/Main.java

--- Lista 1/src/Ex1.java
+++ Lista 1/src/Ex1.java
@@ -16,23 +16,23 @@ public class Ex1 {
         
             for (int i = 0; i < alunos.length; i++) {
                 alunos[i] = new TAluno();
-                System.out.println("Adicionar aluno" + (i + 1) + ":");
+                System.out.println("\nAdicionar aluno " + (i + 1) + ":");
 
-                System.out.print("Nome:");
+                System.out.print("Nome: ");
                 alunos[i].nome = sc.next();
                 if(alunos[i].nome.equalsIgnoreCase("Fim")) 
                     break;
 
-                System.out.print("Idade:");
+                System.out.print("Idade: ");
                 alunos[i].idade = sc.nextInt();
 
-                System.out.print("Endereço:");
+                System.out.print("Endereço: ");
                 alunos[i].endereco = sc.next();
 
-                System.out.print("Data de inscrição:");
+                System.out.print("Data de inscrição: ");
                 alunos[i].dataInscricao = sc.next();
 
-                System.out.print("Data de obtenção");
+                System.out.print("Data de obtenção de carta:");
                 alunos[i].dataObtencaoCarta = sc.next();
                 
             }
@@ -50,6 +50,7 @@ public class Ex1 {
         for (int i = 0; i < alunos.length; i++) {
             if (alunos[i].nome.equalsIgnoreCase("fim") || alunos[i].nome == null) {
                 qntdAlunos = i - 1;
+                break;
             }
         }
 
---
+++ Lista 1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Ex1 exec = new Ex1();
+        exec.CadastroAlunos();
+    }
+}
+
